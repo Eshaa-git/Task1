466493128bba6b76a6bd3f6570299f6b97aa96aa
Esha N <eshwitha@Eshas-MacBook-Air.local>
2025-04-01 14:43:24 -0500

    JIRA-1234 pushing text

---
+++ list.sh
@@ -0,0 +1,10 @@
+cd task1 
+git checkout main 
+ls checklist
+cat checklist
+ls checklist/ > product.txt
+mkdir newdir
+mv product.txt newdir
+git add .
+git commit -m "JIRA-1234 pushing text"
+git push origin main 
