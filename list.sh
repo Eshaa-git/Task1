ab64a4b4dcec956bd13019bc02db30c5ab567aed
Eshaa-git <eshwitha.naini@gmail.com>
2025-04-24 08:19:42 -0500

    Update list.sh

--- list.sh
+++ list.sh
@@ -1,9 +1,9 @@
 git pull origin main
 cd task1 
+mkdir newdir
 git checkout main 
 ls checklist
 ls checklist/ > product.txt
-mkdir newdir
 mv product.txt newdir
 git add .
 git commit -m "JIRA-1234 pushing text"
