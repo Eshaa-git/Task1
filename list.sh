bee443d2912fbfb5d6cfaae33bc6177bfe7dad31
Eshaa-git <eshwitha.naini@gmail.com>
2025-04-24 23:38:59 -0500

    Update list.sh

--- list.sh
+++ list.sh
@@ -1,9 +1,20 @@
-cd task1 
-mkdir newdir
-git checkout main 
-ls checklist
-cd newdir
-ls checklist/ > product.txt
+checklist="https://github.com/Eshaa-git/Task1.git"
+
+# 2. Check if the folder contains any files
+if [ "$(ls -A "$checklist")" ]; then
+
+    # 3. Create a new directory with timestamp to avoid overwriting
+    mkdir "$newdir"
+    newdir="${checklist}"
+
+    # 4. List all filenames and save to a file
+    ls "$checklist" > "${newdir}/checklist.txt"
+
+    echo "Files found and listed in: ${newdir}/checklist.txt"
+
+else
+    echo "No files found in $TARGET_DIR"
+fi
 git add .
 git commit -m"JIRA-1234 pushing text"
 git pull
